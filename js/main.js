63e152fe44c170ab262472d6e4dc6a51e36e3e07
Vivienne Kay <vivkay8@gmail.com>
2015-03-09 19:24:23 -0400

    adding js to tab panels

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var $hamBtn = $('.ham-btn');
 var $panel = $('.panel');
+var $tabs = $('.tabs');
+var $panels = $('.panel');
 
 $hamBtn.on('click', function () {
   $panel.toggleClass('js-panel-open');
 });
+
+$tabs.on('click', 'a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  console.log($panels.filter(':not([hidden])'));
+
+});
+
